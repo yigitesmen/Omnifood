1a4c135cecb1ec996ddd012f481104712d0bc065
Yigit Esmen <yigit.esmen@gmail.com>
2022-11-15 00:38:28 +0300

    Build mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // Set current year
 const year = document.getElementById('year')
 year.textContent = new Date().getFullYear()
+
+// Make mobile navigation work
+const header = document.getElementsByClassName('header')[0]
+const mobileNavBtn = document.getElementsByClassName('mobile-nav-btn')[0]
+mobileNavBtn.addEventListener('click', () => {
+    header.classList.toggle('nav-open')
+})
