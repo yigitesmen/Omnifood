46fdc5e6087c92bfef87cedea42c3cb04cbc2052
Yigit Esmen <yigit.esmen@gmail.com>
2022-11-23 22:15:20 +0300

    Make main navigation work

--- js/script.js
+++ js/script.js
@@ -8,3 +8,9 @@ const mobileNavBtn = document.getElementsByClassName('mobile-nav-btn')[0]
 mobileNavBtn.addEventListener('click', () => {
     header.classList.toggle('nav-open')
 })
+const mainNavLinks = document.getElementsByClassName('main-nav-link')
+for (const link of mainNavLinks) {
+    link.addEventListener('click', () => {
+        if (header.classList.contains('nav-open')) header.classList.remove('nav-open')
+    })
+}
